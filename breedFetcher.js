e4705f8880ff42c4ccbea41d1888fc45855e2b15
Milton Vandol Goh <miltonvandol@gmail.com>
2022-04-19 16:40:17 -0700

    Refactored by moving main request logic into a function to call the callback with either an error or null and test done using Mocha and Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,25 @@
 const request = require('request');
-const agrs = process.argv.slice(2);
-const breedName = agrs[0];
 
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-request(url, (error, response, body) => {
-  console.log('statusCode:', response.statusCode);
-  if (response.statusCode !== 200) {
-    console.log("URL is undefined, please check properly!");
-    return;
-  } else if (body === '[]') {
-    console.log(`Breed is not found, please check BreedName properly again!`);
-    return;
-  } else {
+const fetchBreedDescription = (breedName, callback) => {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    // console.log('statusCode:', response.statusCode);
+    if (error) {
+      callback(error, null);
+      return; // Return is only required for the first one
+    }
     const data = JSON.parse(body);
-    console.log(data[0].description);
-    return;
-  }
-});
+    const breed = data[0];
+    if (response.statusCode !== 200) {
+      callback("URL is invalid, please check again!", null);
+    } else if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback(`Breed is not found, please check BreedName properly again!`, null);
+    }
+  });
+};
 
+module.exports = { fetchBreedDescription };
 
 
